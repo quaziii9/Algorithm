8ce89eee8ccb79c851328a87872f6f1f43c7be7e
quaziii9gamedev@gmail.com <quaziii9gamedev@gmail.com>
2024-03-11 17:56:50 +0900

    초기 커밋.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+            int lastTick = 0;
+            while(true)
+            {
+                #region 프레임 관리
+                // FPS 프레임(60프레임 OK, 30프레임 이하 NO)
+
+                int currenTick = System.Environment.TickCount; // 현재 시간, 시스템이 시작된 이후의 경과된 m/s 
+                // int elapsedTick = currenTick - lastTick; // 경과한 시간 = 현재 시간 - 지난 시간
+
+                // 만약에 경과한 시간이 1 * 1000/30초보다 작다면 (밀리 새컨드 단위라서 * 1000)
+                if (currenTick - lastTick < WAIT_TICK) continue;
+
+                lastTick = currenTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0,0);
+                for(int i = 0; i<25; i++)
+                {
+                    for(int j =0; j<25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }        
+        }
+    }
+}
